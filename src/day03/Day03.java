56f05fec23c591ebaf234a982544a7c500edfd9f
VictorRotha <Victor.Rotha@t-online.de>
2021-12-03 11:35:56 +0100

    Day 03 Part 2

--- src/day03/Day03.java
+++ src/day03/Day03.java
@@ -11,26 +11,78 @@ public class Day03 {
 
     public static void main(String[] args) {
 
-        String path = "src/day03/input_sample.txt";
-        int b = 5;
+        String path = "src/day03/input.txt";
+        int b = 12;
 
         List<Integer> report = getInput(path);
 
-        int[] count = new int[b];
+        part01(report, b);
+
+        int ox = getRating(report, b, "ox");
+        int co = getRating(report, b, "co");
+
+        System.out.printf("Part02\noxygen %s co2 %s\nlife support %s\n", ox, co, ox*co);
+
+        }
+
+        public static int getRating(List<Integer> report, int bLength, String mode) {
+
+            int pos = bLength-1;
+            List<Integer> sample = report;
+            List<Integer> nextSample = new ArrayList<>();
+            while (pos >= 0) {
+                int criteria = getMostCommon(sample, pos);
+                for (int number : sample) {
+                    int bit = ((number & (int) Math.pow(2, pos)) > 0) ? 1 : 0;
+                    if ((mode.equals("ox") && criteria == bit) || (mode.equals("co") && criteria != bit) ) {
+                        nextSample.add(number);
+                    }
+                }
+
+                if (nextSample.size() == 1) {
+                    return nextSample.get(0);
+                }
+
+                sample = nextSample;
+                nextSample = new ArrayList<>();
+                pos--;
+
+            }
+
+            return -1;
+
+        }
+
+    public static int getMostCommon(List<Integer> sample, int pos) {
+
+        int result = 0;
+
+        for (int n: sample) {
+            int p = (int) Math.pow(2, pos);
+            if ((n&p) > 0)  result++;
+        }
+
+        if (result >= sample.size() - result)
+            return 1;
+        else
+            return 0;
+    }
 
 
+    public static void part01(List<Integer> report, int bLength) {
+
+        int[] count = new int[bLength];
 
         for (int n : report) {
-            for (int j = 0; j < b; j++) {
+            for (int j = 0; j < bLength; j++) {
                 count[j] += n & 1;
                 n = n >> 1;
             }
-
         }
 
         int gamma = 0;
         int epsilon = 0;
-        for (int j = 0; j < b; j++) {
+        for (int j = 0; j < bLength; j++) {
             if (count[j] > report.size() / 2) {
                 gamma += Math.pow(2, j);
             } else {
@@ -39,9 +91,8 @@ public class Day03 {
 
         }
 
-        System.out.printf("reort size %s, binary length %s\ngamma %s, epsilon %s, product %s \n",
-                report.size(), b, gamma, epsilon, gamma*epsilon);
-
+        System.out.printf("Part01\ngamma %s, epsilon %s\npower consumption %s \n\n",
+                gamma, epsilon, gamma*epsilon);
     }
 
 
