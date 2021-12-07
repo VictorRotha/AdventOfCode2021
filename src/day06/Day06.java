c9387a4c2bb8f5497efbdd9ebf46c5d6316eb9c4
VictorRotha <Victor.Rotha@t-online.de>
2021-12-07 10:19:25 +0100

    Day 06 Part 2

--- src/day06/Day06.java
+++ src/day06/Day06.java
@@ -1,6 +1,7 @@
 package day06;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Day06 {
 
@@ -14,6 +15,20 @@ public class Day06 {
         }
 
         int days = 80;
+        int allfishes = getAllFishes(days, fishes);
+
+        System.out.printf("\nNumber of Fishes after %3d days: %15d", days, allfishes);
+
+        days = 256;
+        long allfishesLong = getAllFishesLong(days, fishes);
+
+        System.out.printf("\nNumber of Fishes after %3d days: %15d", days, allfishesLong);
+    }
+
+    public static int getAllFishes(int days, ArrayList<Integer> fishes) {
+
+        fishes = new ArrayList<>(fishes);
+
         for (int day = 0; day < days; day++) {
 
             int newFishes = 0;
@@ -31,10 +46,46 @@ public class Day06 {
             for (int i = 0; i < newFishes; i++) {
                 fishes.add(8);
             }
-
         }
 
-        System.out.printf("\nNumber of Fishes after %s days: %s\n", days, fishes.size());
-
+        return fishes.size();
     }
+
+
+    public static long getAllFishesLong(int days, ArrayList<Integer> fishes) {
+
+        HashMap<Integer, Long> states = new HashMap<>();
+        states.put(0, 0L);
+        states.put(1, 0L);
+        states.put(2, 0L);
+        states.put(3, 0L);
+        states.put(4, 0L);
+        states.put(5, 0L);
+        states.put(6, 0L);
+        states.put(7, 0L);
+        states.put(8, 0L);
+
+        for (Integer fish : fishes) {
+            states.put(fish, states.get(fish)+1);
+        }
+
+        for (int i = 0; i < days; i++) {
+
+            long newFish = states.get(0);
+            for (int j = 0; j < 8; j++ ) {
+                states.put(j, states.get(j + 1));
+            }
+            states.put(6, states.get(6) + newFish);
+            states.put(8, newFish);
+        }
+
+        long sum = 0;
+        for (int i = 0; i <= 8; i++) {
+            sum += states.get(i);
+        }
+
+        return sum;
+    }
+
+
 }
