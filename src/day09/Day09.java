d7e5cb9a2513e8fcf1740233b77af0e00fc4faa6
VictorRotha <Victor.Rotha@t-online.de>
2021-12-09 14:14:38 +0100

    Day 09 Part 2

--- src/day09/Day09.java
+++ src/day09/Day09.java
@@ -1,11 +1,10 @@
 package day09;
 
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-
+import java.util.*;
 
 public class Day09 {
 
@@ -15,34 +14,122 @@ public class Day09 {
 
         ArrayList<Integer[]> map = getInput(path);
 
+        List<Integer[]> lowPoints = getLowPoints(map);
 
-        ArrayList<Integer> lowValues = new ArrayList<>();
+        int riskSum  = 0;
+        for (Integer[] point : lowPoints) {
+            riskSum += 1 + map.get(point[0])[point[1]];
+        }
 
-//        for (Integer[] row : map) System.out.println(Arrays.toString(row));
-//        System.out.println();
+        System.out.println("Risksum: " + riskSum);
+
+
+        boolean[][] checked = new boolean[map.size()][map.get(0).length];
+
+        ArrayList<Integer> basinSizes = new ArrayList<>();
+        for (Integer[] point : lowPoints) {
+            basinSizes.add(getBasin(map, point[0], point[1], checked).size());
+        }
+
+        Collections.sort(basinSizes);
+
+        int s = basinSizes.size();
+        int product = basinSizes.get(s - 1) * basinSizes.get(s - 2) * basinSizes.get(s - 3);
+
+        System.out.println("Product 3 largest basins: " + product);
+
+    }
+
+    public static ArrayList<Integer[]> getBasin(ArrayList<Integer[]> map, int row, int col, boolean[][] checked) {
+
+        ArrayList<Integer[]> basin = new ArrayList<>();
+
+        if (checked[row][col]) return basin;
+
+        basin.add(new Integer[]{row, col});
+        checked[row][col] = true;
+
+        List<Integer[]> adj = findValidAdjacents(map, row, col, checked);
+
+        for (Integer[] a : adj) {
+            basin.addAll(getBasin(map, a[0], a[1], checked));
+        }
+        return basin;
+
+    }
+
+    public static List<Integer[]> findValidAdjacents(ArrayList<Integer[]> map, int row, int col, boolean[][] checked) {
+
+        ArrayList<Integer[]> adj = new ArrayList<>();
+
+        int arow;
+        int acol;
+
+        if (row > 0) {
+
+            arow = row - 1;
+            acol = col;
+
+            if (map.get(arow)[acol] != 9 && !checked[arow][acol]) {
+                adj.add(new Integer[] {arow, acol});
+            }
+
+        }
+
+        if (row < map.size() - 1) {
+
+            arow = row + 1;
+            acol = col;
+
+            if (map.get(arow)[acol] != 9 && !checked[arow][acol]) {
+                adj.add(new Integer[] {arow, acol});
+            }
+
+        }
+
+        if (col > 0) {
+
+            arow = row;
+            acol = col - 1;
+
+            if (map.get(arow)[acol] != 9 && !checked[arow][acol]) {
+                adj.add(new Integer[]{arow, acol});
+            }
+        }
+
+        if (col < map.get(row).length-1) {
+
+            arow = row;
+            acol = col + 1;
+
+            if (map.get(arow)[acol] != 9 && !checked[arow][acol]) {
+                adj.add(new Integer[]{arow, acol});
+            }
+        }
+
+        return adj;
+    }
+
+    public static List<Integer[]> getLowPoints(ArrayList<Integer[]> map) {
+
+        ArrayList<Integer[]> lowPoints = new ArrayList<>();
 
         for (int row = 0; row < map.size(); row++) {
             for (int col = 0; col < map.get(row).length; col++) {
 
                 int value = map.get(row)[col];
-                ArrayList<Integer> adj = getAdjacents(map, row, col);
+                ArrayList<Integer> adj = getAdjacentValues(map, row, col);
 
-                if (value < Collections.min(adj)) lowValues.add(value);
-
-//                System.out.printf("%s  %s  %s %s  %s  %s\n", row, col, v, adj, Collections.min(adj), ((islowest) ? "lowpoint" : ""));
-
+                if (value < Collections.min(adj)) lowPoints.add(new Integer[] {row, col});
             }
+
         }
 
-        int risksum = 0;
-        for (int low : lowValues) risksum += 1 + low;
-
-        System.out.println("risksum   " + risksum);
-
+        return lowPoints;
     }
 
 
-    public static ArrayList<Integer> getAdjacents( ArrayList<Integer[]> map, int row, int col) {
+    public static ArrayList<Integer> getAdjacentValues( ArrayList<Integer[]> map, int row, int col) {
 
         ArrayList<Integer> adj = new ArrayList<>();
 
@@ -56,7 +143,6 @@ public class Day09 {
             adj.add(map.get(row)[col + 1]);
 
         return adj;
-
     }
 
 
