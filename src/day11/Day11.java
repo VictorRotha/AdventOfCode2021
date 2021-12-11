7d7fbc7011fdd1d30b52576686130803f9247996
VictorRotha <Victor.Rotha@t-online.de>
2021-12-11 11:50:32 +0100

    Day 11 Part 2

--- src/day11/Day11.java
+++ src/day11/Day11.java
@@ -13,58 +13,59 @@ public class Day11 {
         String path = "src/day11/input.txt";
 
         int[][] grid = getInput(path);
-
-//        for (int[] row : grid) {
-//            System.out.println(Arrays.toString(row));
-//        }
+        int[][] gridCopy = Arrays.stream(grid).map(int[]::clone).toArray(int[][]::new);
 
         int steps = 100;
         int total = 0;
         for (int step = 0 ; step < steps; step++) {
-
-            for (int y = 0; y < grid.length; y++) {
-                int[] row = grid[y];
-                for (int x = 0; x < row.length; x++) {
-                    addEnergy(grid, y, x);
-                }
-            }
-
-            int flashes = 0;
-            for (int y = 0; y < grid.length; y++) {
-                for (int x = 0; x < grid[y].length; x++) {
-                    if (grid[y][x] == -1) {
-                        grid[y][x] = 0;
-                        flashes++;
-                    }
-                }
-            }
-
-            total += flashes;
-
-            /*
-
-            System.out.println("\nStep " + (step+1));
-            System.out.println("flashes " + flashes + " total " + total);
-
-            System.out.println();
-            for (int[] row : grid) {
-                System.out.println(Arrays.toString(row));
-            }
-
-             */
-
+            total += addEnergyToGrid(Arrays.copyOf(gridCopy, gridCopy.length));
         }
 
-        System.out.printf("total %s flashes after %s steps\n", total, steps );
+        System.out.printf("total flashes after %s steps: %s\n", steps, total);
+
+        boolean sync = false;
+        int step = 0;
+        int gridSize = grid.length * grid[0].length;
+        while (!sync) {
+            step++;
+            int flashes = addEnergyToGrid(grid);
+            if (flashes == gridSize) sync = true;
+        }
+
+        System.out.printf("synchronized after %s steps\n", step);
 
     }
+
+
+    public static int addEnergyToGrid(int[][] grid) {
+
+        for (int y = 0; y < grid.length; y++) {
+            int[] row = grid[y];
+            for (int x = 0; x < row.length; x++) {
+                addEnergy(grid, y, x);
+            }
+        }
+
+        int flashes = 0;
+        for (int y = 0; y < grid.length; y++) {
+            for (int x = 0; x < grid[y].length; x++) {
+                if (grid[y][x] == -1) {
+                    grid[y][x] = 0;
+                    flashes++;
+                }
+            }
+        }
+
+        return flashes;
+    }
+
 
     public static void addEnergy(int[][] grid, int y, int x) {
 
         int v = grid[y][x];
         if (v != -1) v++;
         if (v > 9) {
-            grid[y][x] = -1;;
+            grid[y][x] = -1;
             ArrayList<int[]> adjacents = findAdjacents(grid, y, x);
             for (int[] adj : adjacents) {
                 addEnergy(grid, adj[0], adj[1]);
@@ -86,19 +87,16 @@ public class Day11 {
         int w = grid[0].length;
 
         for (int i = 0; i < 8; i++) {
-
             int ax = x + adjX[i];
             int ay = y + adjY[i];
-
             if (ax >= 0 && ay >= 0 && ax < w && ay < h && grid[ay][ax] != -1) {
-
                 result.add(new int[]{ay, ax});
             }
         }
 
         return result;
-
     }
+
 
     public static int[][] getInput(String path) {
 
@@ -112,7 +110,6 @@ public class Day11 {
                 int[] row = new int[line.strip().length()];
                 for (int i = 0; i < line.strip().length(); i++) {
                     row[i] = Integer.parseInt(String.valueOf(line.charAt(i)));
-
                 }
                 lines.add(row);
 
@@ -123,14 +120,7 @@ public class Day11 {
             e.printStackTrace();
         }
 
-        int[][] result = new int[lines.size()][lines.get(0).length];
-        for (int i = 0; i < lines.size(); i++) {
-            result[i] = lines.get(i);
-        }
-
-        return result;
-
-
+        return lines.stream().toArray(int[][]::new);
     }
 
 
